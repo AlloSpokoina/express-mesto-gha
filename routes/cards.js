64877766da6133acad2bfa71553b9109c85f3037
Valeriya Markina <118906987+AlloSpokoina@users.noreply.github.com>
2023-07-31 04:25:16 +0500

    fix: syntax error

--- routes/cards.js
+++ routes/cards.js
@@ -6,7 +6,7 @@ const {
 router.get('/', getCards);
 router.delete('/:cardId', deleteCard);
 router.post('/', addCard);
-router.put('/cardId/likes', likeCard);
+router.put('/:cardId/likes', likeCard);
 router.delete('/:cardId/likes', dislikeCard);
 
 module.exports = router;
