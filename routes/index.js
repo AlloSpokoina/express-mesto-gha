a36dff6e7c93d8d8d194f80a782bf0d6387b3a3b
Valeriya Markina <118906987+AlloSpokoina@users.noreply.github.com>
2023-08-15 20:51:31 +0500

    test

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,4 @@
 const router = require('express').Router();
-// const userRouter = require('./users');
-// const cardsRouter = require('./cards');
 const signupRouter = require('./signup');
 const signinRouter = require('./signin');
 const auth = require('../middlewares/auth');
@@ -8,7 +6,5 @@ const auth = require('../middlewares/auth');
 router.use('/signup', signupRouter);
 router.use('/signin', signinRouter);
 router.use(auth);
-// router.use('/usars', userRouter);
-// router.use('/cards', cardsRouter);
 
 module.exports = router;
