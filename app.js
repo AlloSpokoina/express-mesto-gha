cf6e4c533340a37fd548a4addef74458a92ee0d3
Valeriya Markina <118906987+AlloSpokoina@users.noreply.github.com>
2023-08-15 15:42:51 +0500

    after 1 iteration ✔️

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 const rateLimiter = require('express-rate-limit');
-const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const NotfoundError = require('./error/NotFoundError');
 
@@ -10,20 +9,12 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(helmet());
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
-
-app.use('*', (req, res, next) => {
-  next(new NotfoundError('Запрашиваемый ресурс не найден'));
 });
 
 const limiter = rateLimiter({
@@ -32,6 +23,14 @@ const limiter = rateLimiter({
   message: "You can't make any more requests at the moment. Try again later",
 });
 app.use(limiter);
+
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
+app.use('*', (req, res, next) => {
+  next(new NotfoundError('Запрашиваемый ресурс не найден'));
+});
 
 app.use(errors());
 
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,14 +6,16 @@ const { SECRET_KEY = 'mestogha' } = process.env;
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnautorizedError('Необходима авторизация');
+    next(new UnautorizedError('Необходима авторизация'));
+    return;
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, SECRET_KEY);
   } catch (err) {
-    throw new UnautorizedError('Необходима авторизация');
+    next(new UnautorizedError('Необходима авторизация'));
+    return;
   }
   req.user = payload;
   next();
