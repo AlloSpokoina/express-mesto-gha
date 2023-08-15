946d26b94a916707a316608419d9482c98386f96
Valeriya Markina <118906987+AlloSpokoina@users.noreply.github.com>
2023-08-15 19:11:46 +0500

    fix test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,16 +6,14 @@ const { SECRET_KEY = 'mestogha' } = process.env;
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new UnautorizedError('Необходима авторизация'));
-    return;
+    throw new UnautorizedError('Необходима авторизация');
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, SECRET_KEY);
   } catch (err) {
-    next(new UnautorizedError('Необходима авторизация'));
-    return;
+    throw new UnautorizedError('Необходима авторизация');
   }
   req.user = payload;
   next();
