cd8714943ef7e611b240a38e51e6f59507d6218a
Valeriya Markina <118906987+AlloSpokoina@users.noreply.github.com>
2023-08-15 16:00:44 +0500

    fix test1

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.en
 
 const app = express();
 
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 
